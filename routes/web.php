6a0ee223437687364060c14d411508be9a34890a
mined <determined_xw@126.com>
2019-07-04 16:41:48 +0800

    测试

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/phpinfo', function () {
-    return view('phpinfo');
+    return view('phpinfo'); 
 });
+
+
 
 // 前台首页
 Route::get('/login', 'Home\Auth\LoginController@index');
